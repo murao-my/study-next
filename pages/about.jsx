230ad0b012092bbcdbb75470cf9ff762c52126fb
murao-my <kamitukumikio@gmail.com>
2023-01-15 17:48:20 +0900

    🐛 _app.jsxにHeadをimportするように変更

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+
 import Header from "../components/Header";
 import Main from "../components/Main";
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+
 import Main from "../components/Main";
 import Header from "../components/Header";
 export default function Home() {
